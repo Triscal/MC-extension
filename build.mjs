54bfdeb6e488583e7bb7cb83fc534e5bb05c3708
Triscal <66183943+Triscal@users.noreply.github.com>
2026-06-09 20:12:34 -0400

    updating icon and rename

--- build.mjs
+++ build.mjs
@@ -12,13 +12,22 @@ await esbuild.build({
 });
 
 // Copy static files
-const staticFiles = ["icon.png", "manifest.json", "options.html", "popup.html"];
+const staticFiles = ["manifest.json", "options.html", "popup.html"];
 
 fs.mkdirSync("dist", { recursive: true });
 
 for (const file of staticFiles) {
   fs.copyFileSync(file, path.join("dist", path.basename(file)));
   console.log(`Copied ${file} → dist/`);
+}
+
+const icons = ["icons/icon.png","icons/icon128.png", "icons/icon16.png", "icons/icon32.png", "icons/icon48.png"]
+
+fs.mkdirSync("dist/icons", { recursive: true });
+
+for (const icon of icons) {
+  fs.copyFileSync(icon, path.join("dist/icons", path.basename(icon)));
+  console.log(`Copied ${icon} → dist/icons`);
 }
 
 console.log("Build complete.");
